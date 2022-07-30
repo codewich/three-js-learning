509cfe8d211eb5fd9519005fe886a607599a9b9a
Michael Lin <linzhiming111@gmail.com>
2022-07-30 14:35:24 -0400

    day1 basic scene

---
+++ script.js
@@ -0,0 +1,27 @@
+const scene = new THREE.Scene()
+
+//red cube
+const geometry = new THREE.BoxGeometry(1, 1, 1)
+const material = new THREE.MeshBasicMaterial({color: 0xff0000})
+const mesh = new THREE.Mesh(geometry, material)
+scene.add(mesh)
+
+//Sizes 
+const sizes = {
+    witdth: 800,
+    height: 600
+}
+
+// camera
+const camera = new THREE.PerspectiveCamera(75, sizes.witdth / sizes.height)
+camera.position.z = 1.5
+scene.add(camera)
+
+//renderer
+const canvas = document.querySelector('.webgl')
+const renderer = new THREE.WebGLRenderer({
+    canvas: canvas
+})
+renderer.setSize(sizes.witdth, sizes.height)
+
+renderer.render(scene, camera)
